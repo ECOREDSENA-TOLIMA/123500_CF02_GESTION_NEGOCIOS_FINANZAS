8fbe4505f0194c67ecf00cc572acfa2845808ea8
Oscar Espitia <odespitia@sena.edu.co>
2024-05-24 07:14:46 -0500

    empaqutedo

--- src/config/global.js
+++ src/config/global.js
@@ -112,7 +112,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CFA_02_123500.zip',
       },
       {
         icono: 'fas fa-download',
